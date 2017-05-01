be6ac9abba74610f233374c21a23ada09f0e0b33
Cheburaxter <derebah@gmail.com>
2017-05-01 22:11:26 +0300

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+	using namespace std;
+
+int main(void) {
+	
+	cout << "Hello World!\n";
+	cin.get();
+	//_getch();
+	return 0;
+}
---
+++ HelloWorld2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(void) {
+
+	cout << "sadsd\n";
+
+	_getch();
+	return 0;
+}
